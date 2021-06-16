c4cd34e79186eb2ce2b453098d8982da92387875
Héctor Santamaría <hector.stamaria92@gmail.com>
2021-06-15 19:55:34 -0500

    X

--- src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/service/DespachoPIService.java
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/service/DespachoPIService.java
@@ -1,6 +1,7 @@
 package com.mx.totalplay.ffm.cloudweb.plantainterna.service;
 
 import com.mx.totalplay.ffm.cloudweb.utilerias.model.ServiceResponseResult;
+
 public interface DespachoPIService {
 
 	public ServiceResponseResult consultarCatalogoAccionesOTPI();
