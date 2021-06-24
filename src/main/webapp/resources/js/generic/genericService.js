645a21ff280cc73a28cda2c934197aa01776d5b3
Jose Maria Portillo Alvarez <portilloright@gmail.com>
2021-06-24 18:15:46 -0500

    catalogo intervenciones y catalogo geografia

--- src/main/webapp/resources/js/generic/genericService.js
+++ src/main/webapp/resources/js/generic/genericService.js
@@ -1,13 +1,29 @@
-app.service("genericService", function($http){
-	
+app.service("genericService", function ($http) {
+
     this.consultarCatalogo = function (params) {
         return $http({
             method: "post",
             url: "req/consultarCatalogoDesphachoPI",
             data: params,
-            headers: {'Content-Type': "application/json; charset=utf-8"},
+            headers: { 'Content-Type': "application/json; charset=utf-8" },
             transformRequest: angular.identity
         });
     };
-	
+
+    this.consultarCatalogoIntervenciones = function () {
+        return $http({
+            method: "post",
+            url: "req/consultarCatalogoTipoOrdenUsuarioDespacho",
+            headers: { 'Content-Type': "application/json; charset=utf-8" }
+        });
+    };
+
+    this.consulCatalogoGeografia=function(){
+		return $http({
+			method: "post",
+			url: "req/consulCatalogoGeografiaUsuarioDespacho",
+            headers: { 'Content-Type': "application/json; charset=utf-8" }
+		});
+	};
+
 })
