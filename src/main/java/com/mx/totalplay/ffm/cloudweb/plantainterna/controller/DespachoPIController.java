edde45fde1ac658d916e2ad591c47988820a2d56
Héctor Santamaría <hector.stamaria92@gmail.com>
2021-06-29 13:08:43 -0500

    Se agrega commit de LOG

--- src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/controller/DespachoPIController.java
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/controller/DespachoPIController.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import com.mx.totalplay.ffm.cloudweb.plantainterna.service.DespachoPIService;
 import com.mx.totalplay.ffm.cloudweb.utilerias.model.ServiceResponseResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 @RequestMapping("/req")
 public class DespachoPIController {
-	private  final Logger logger = LogManager.getLogger(DespachoPIController.class.getName());
+    private static final Logger LOGGER = LogManager.getLogger(DespachoPIController.class);
 
 	private DespachoPIService despachoService;
 	
@@ -25,37 +25,37 @@ public class DespachoPIController {
 	
 	@PostMapping("/consultarCatalogoDesphachoPI")
     public ServiceResponseResult consultarCatalogoDesphachoPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO CATALGOS");
+		LOGGER.info("##### CONSULTANDO CATALGOS");
 		ServiceResponseResult response = despachoService.consultarCatalogosPI(params);
         return response;
     }
 	@PostMapping("consulCatalogoGeografiaGeneralDespacho")
 	public ServiceResponseResult consulCatalogoGeografiaGeneralDespacho() {
-		logger.info("##### CONSULTANDO CATALOGO GEOGRAFIA GENERAL DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO GEOGRAFIA GENERAL DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoGeografiaGeneral(  );
         return response;
 	}
 	@PostMapping("consulCatalogoGeografiaUsuarioDespacho")
 	public ServiceResponseResult consulCatalogoGeografiaUsuarioDespacho() {
-		logger.info("##### CONSULTANDO CATALOGO GEOGRAFIA USUARIO DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO GEOGRAFIA USUARIO DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoGeografiaUsuario(  );
         return response;
 	}		
 	@PostMapping("consultarCatalogoTipoOrdenGeneralDespacho")
 	public ServiceResponseResult consultarCatalogoTipoOrdenGeneralDespacho() {
-		logger.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoTipoOrdenGeneralDespacho(  );
         return response;
 	}		
 	@PostMapping("consultarCatalogoTipoOrdenUsuarioDespacho")
 	public ServiceResponseResult consultarCatalogoTipoOrdenUsuarioDespacho() {
-		logger.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoTipoOrdenUsuarioDespacho(  );
         return response;
 	}			
 	@PostMapping("consultarCatalogoEstatusDespachoPI")
 	public ServiceResponseResult consultarCatalogoEstatusOrdenDespacho() {
-		logger.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO ESTATUS DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoEstatusOrden(  );
         return response;
 	}
@@ -63,7 +63,7 @@ public class DespachoPIController {
 	
 	@PostMapping("consultarCatalogoTurnosDespachoPI")
 	public ServiceResponseResult consultarCatalogoTurnosPI() {
-		logger.info("##### CONSULTANDO CATALOGO TURNO DESPACHO PI");
+		LOGGER.info("##### CONSULTANDO CATALOGO TURNO DESPACHO PI");
 		ServiceResponseResult response = despachoService.consultarCatalogoTurnosPI(  );
         return response;
 	}
@@ -71,21 +71,21 @@ public class DespachoPIController {
 	
 	@PostMapping("consultarHistoricoDespachoOT")
 	public ServiceResponseResult consultarHistoricoDespachoOTCon(@RequestBody String params) {
-		logger.info("##### CONSULTANDO HISTORICO DESPACHO OT");
+		LOGGER.info("##### CONSULTANDO HISTORICO DESPACHO OT");
 		ServiceResponseResult response = despachoService.consultarHistoricoOTPI( params );
         return response;
 	}
 	
 	@PostMapping("consultarDetalleDespachoOT")
 	public ServiceResponseResult consultarDetalleDespachoOTCon(@RequestBody String params) {
-		logger.info("##### CONSULTANDO DETALLE OT DESPACHO");
+		LOGGER.info("##### CONSULTANDO DETALLE OT DESPACHO");
 		ServiceResponseResult response = despachoService.consultarDetalleOTPI( params );
         return response;
 	}
 	
 	@PostMapping("consultarComentariosDespachoOT")
 	public ServiceResponseResult consultarComentariosDespachoOTCon(@RequestBody String params) {
-		logger.info("##### CONSULTANDO COMENTARIOS ");
+		LOGGER.info("##### CONSULTANDO COMENTARIOS ");
 		ServiceResponseResult response = despachoService.consultarComentariosOTPI( params );
         return response;
 	}
@@ -93,28 +93,28 @@ public class DespachoPIController {
 	
 	@PostMapping("cambiarEstatusOrdenPI")
 	public ServiceResponseResult cambiarEstatusOrdenTrabajo(@RequestBody String params) {
-		logger.info("##### CAMBIANDO ESTATUS ORDEN TRABAJO---");
+		LOGGER.info("##### CAMBIANDO ESTATUS ORDEN TRABAJO---");
 		ServiceResponseResult response = despachoService.cambiarEstatusOrdenTrabajo( params );
         return response;
 	}
 	
 	@PostMapping("consultandoMaterialesPI")
 	public ServiceResponseResult consultarMaterialesDespachoOper(@RequestBody String params) {
-		logger.info("##### CONSULTANDO MATERIALES PI OPERARIO");
+		LOGGER.info("##### CONSULTANDO MATERIALES PI OPERARIO");
 		ServiceResponseResult response = despachoService.consultarMaterialesOperarioPI( params );
         return response;
 	}
 	
 	@PostMapping("consultarVehiculoOperarioPI")
 	public ServiceResponseResult consultarVehiculoOperarioPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO INFORMAICON VEHICULO");
+		LOGGER.info("##### CONSULTANDO INFORMAICON VEHICULO");
 		ServiceResponseResult response = despachoService.cambiarEstatusTecnicoDespachoPI( params );
         return response;
 	}
 	
 	@PostMapping("consultarOtsTrabajadasDespacho")
 	public ServiceResponseResult consultarOtsTrabajadasDespacho(@RequestBody String params) {
-		logger.info("##### CONSULTANDO OTS TRABAJADAS");
+		LOGGER.info("##### CONSULTANDO OTS TRABAJADAS");
 
 		ServiceResponseResult response = despachoService.cambiarEstatusTecnicoDespachoPI( params );
         return response;
@@ -122,28 +122,28 @@ public class DespachoPIController {
 	
 	@PostMapping("cambiarEstatusOperarioPI")
 	public ServiceResponseResult cambiarEstatusOperarioPI(@RequestBody String params) {
-		logger.info("##### CAMBIANDO ESTATUS TECNICO PI");
+		LOGGER.info("##### CAMBIANDO ESTATUS TECNICO PI");
 		ServiceResponseResult response = despachoService.cambiarEstatusTecnicoDespachoPI( params );
         return response;
 	}
 	
 	@PostMapping("/consultarCatalogoEstatusTecnico")
 	public ServiceResponseResult consultarCatalogoEstatusTecnico(@RequestBody String params) {
-		logger.info("##### CONSULTANDO CATALOGO ESTATUS TECNICO");
+		LOGGER.info("##### CONSULTANDO CATALOGO ESTATUS TECNICO");
 		ServiceResponseResult response = despachoService.consultarCatalogoEstatusTecnico( params );
         return response;
     }
 	
 	@PostMapping("/consultarConteoAlertasPI")
 	public ServiceResponseResult consultarConteoAlertasPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO CONTEO ALERTAS");
+		LOGGER.info("##### CONSULTANDO CONTEO ALERTAS");
 		ServiceResponseResult response = despachoService.consultarConteoAlertasPI( params );
         return response;
     }
 		
 	@PostMapping("/consultarOrdenesPendientes")
     public ServiceResponseResult consultarOrdenesTrabajoPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO ORDENES PENDIENTES");
+		LOGGER.info("##### CONSULTANDO ORDENES PENDIENTES");
 		ServiceResponseResult response = despachoService.obtenerOrdenesTrabajoPendientesDespacho( params );
         return response;
     }
@@ -151,26 +151,26 @@ public class DespachoPIController {
 
 	@PostMapping("/consultarOtsAsignadas")
 	public ServiceResponseResult consultarOrdenesAsignadas(@RequestBody String params) {				
-		logger.info("##### CONSULTANDO CATALGOS");
+		LOGGER.info("##### CONSULTANDO CATALGOS");
 		ServiceResponseResult response = despachoService.consultarOrdenesAsignadasOperario(params);
         return response;
 	}
 	@PostMapping("/consultarTecnicosDisponiblesDespachoPI")
 	public ServiceResponseResult consultarTecnicosDisponiblesDespachoPI() {				
-		logger.info("##### CONSULTANDO OPERARIOS DISPONIBLES");
+		LOGGER.info("##### CONSULTANDO OPERARIOS DISPONIBLES");
 		ServiceResponseResult response = despachoService.consultarOperariosAsignadosDespacho();
         return response;
 	}
 	
 	@PostMapping("/consultarPaletaColores")
 	public ServiceResponseResult consultarPaletaColoresController() {				
-		logger.info("##### CONSULTANDO PALETA COLORES");
+		LOGGER.info("##### CONSULTANDO PALETA COLORES");
 		ServiceResponseResult response = despachoService.consultarColoresIconografia();
         return response;
 	}
 	@PostMapping("/consultarCatalogoAccionesDespachoPI")
 	public ServiceResponseResult consultarCatalogoAcciones() {				
-		logger.info("##### CONSULTANDO consultarCatalogoAcciones");
+		LOGGER.info("##### CONSULTANDO consultarCatalogoAcciones");
 		ServiceResponseResult response = despachoService.consultarCatalogoAccionesOTPI();
         return response;
 	}
@@ -178,63 +178,63 @@ public class DespachoPIController {
 	
 	@PostMapping("/getDetalleAlertas")
 	public ServiceResponseResult getDetalleAlertas(@RequestBody String params) {
-		logger.info("##### CONSULTANDO getDetalleAlertas");
+		LOGGER.info("##### CONSULTANDO getDetalleAlertas");
 		ServiceResponseResult response = despachoService.getDetalleAlertas(params);
 		return response;
 	}
 	
 	@PostMapping("/consultaAccionesAlerta")
 	public ServiceResponseResult consultaAccionesAlerta(@RequestBody String params) {
-		logger.info("##### CONSULTANDO consultaAccionesAlerta");
+		LOGGER.info("##### CONSULTANDO consultaAccionesAlerta");
 		ServiceResponseResult response = despachoService.consultaAcciones(params);
 		return response;
 	}
 	
 	@PostMapping("/getCatalogoStatusEstadoMotivo")
 	public ServiceResponseResult getCatalogoStatusEstadoMotivo(@RequestBody String params) {
-		logger.info("##### CONSULTANDO getDetalleAlertas");
+		LOGGER.info("##### CONSULTANDO getDetalleAlertas");
 		ServiceResponseResult response = despachoService.getCatalogoStatusEstadoMotivo(params);
 		return response;
 	}
 	
 	@PostMapping("/consultarEvidenciaAlertaPI")
 	public ServiceResponseResult consultarEvidenciaAlertaPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO consultarEvidenciaAlertaPI");
+		LOGGER.info("##### CONSULTANDO consultarEvidenciaAlertaPI");
 		ServiceResponseResult response = despachoService.consultarEvidenciaAlertaPI(params);
 		return response;
 	}
 	
 	@PostMapping("/consultarHistoricoAlertaPI")
 	public ServiceResponseResult consultarHistoricoAlertaPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO consultarHistoricoAlertaPI");
+		LOGGER.info("##### CONSULTANDO consultarHistoricoAlertaPI");
 		ServiceResponseResult response = despachoService.consultarHistoricoAlertaPI(params);
 		return response;
 	}
 	
 	@PostMapping("/consultarComentariosAlertaPI")
 	public ServiceResponseResult consultarComentariosAlertaPI(@RequestBody String params) {
-		logger.info("##### CONSULTANDO consultarComentariosAlertaPI");
+		LOGGER.info("##### CONSULTANDO consultarComentariosAlertaPI");
 		ServiceResponseResult response = despachoService.consultarComentariosAlertaPI(params);
 		return response;
 	}
 	
 	@PostMapping("/consultarlocalizacionOtPIDespacho")
 	public ServiceResponseResult consultarlocalizacionOtPIDespachoController(@RequestBody String params) {
-		logger.info("##### CONSULTANDO consultarlocalizacionOtPIDespachoController");
+		LOGGER.info("##### CONSULTANDO consultarlocalizacionOtPIDespachoController");
 		ServiceResponseResult response = despachoService.consultarlocalizacionOtPIDespacho(params);
 		return response;
 	}
 	
 	@PostMapping("/cambiarEstatusIntegrador") 
 	  public ServiceResponseResult cambiarEstatusIntegrador(@RequestBody String params) { 
-	    logger.info("##### CONSULTANDO cambiarEstatusIntegrador"); 
+	    LOGGER.info("##### CONSULTANDO cambiarEstatusIntegrador"); 
 	    ServiceResponseResult response = despachoService.cambiarEstatusIntegrador(params); 
 	    return response; 
 	  } 
 	   
 	  @PostMapping("/setComentariosIntegrador") 
 	  public ServiceResponseResult setComentariosIntegrador(@RequestBody String params) { 
-	    logger.info("##### CONSULTANDO setComentariosIntegrador"); 
+	    LOGGER.info("##### CONSULTANDO setComentariosIntegrador"); 
 	    ServiceResponseResult response = despachoService.setComentariosIntegrador(params); 
 	    return response; 
 	  } 
--- src/main/java/com/mx/totalplay/ffm/cloudweb/utilerias/controller/GenericController.java
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/utilerias/controller/GenericController.java
@@ -1,5 +1,9 @@
 package com.mx.totalplay.ffm.cloudweb.utilerias.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.mx.totalplay.ffm.cloudweb.utilerias.utils.ConstantesGeneric;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class GenericController {
@@ -33,4 +41,29 @@ public class GenericController {
 		mapaVars.put("gkeactok", genericVars.getGoogAccLLaevATok());
 	  return mapaVars;
 	}
+
+	@GetMapping("/detailsHelp")
+	public String redireccionLog(){
+		logger.info("ENTRA DECISION");
+		return "utilerias/help/details";
+	}
+	@GetMapping("/descargaLogFile/{fileName:.+}")
+	public void downloadLoggerFile(HttpServletRequest request, HttpServletResponse response,
+								   @PathVariable("fileName")String fileName){
+		Path pathFile= Paths.get("/u01/ffmcloudlog/",fileName);
+		logger.info("descargandoFile....");
+		if( Files.exists(pathFile)){
+      		response.setContentType("application/octet-stream");
+			response.addHeader("Content-Disposition", "attachment; filename="+fileName);
+			try{
+				Files.copy(pathFile,response.getOutputStream());
+				response.getOutputStream().flush();
+			}catch( IOException ex){
+				ex.printStackTrace();
+			}
+
+		}
+	}
+
+
 }
