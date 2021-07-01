5c167de6f619e5e342a05c3511babe79d6f1e1ca
Héctor Santamaría <hector.stamaria92@gmail.com>
2021-07-01 17:27:21 -0500

    Se cambia configuracion de sesion

---
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/config/security/AppConfig.java
@@ -0,0 +1,2 @@
+package com.mx.totalplay.ffm.cloudweb.config.security;public class AppConfig {
+}
---
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/model/TestingModel.java
@@ -0,0 +1,2 @@
+package com.mx.totalplay.ffm.cloudweb.plantainterna.model;public class TestingModel {
+}
