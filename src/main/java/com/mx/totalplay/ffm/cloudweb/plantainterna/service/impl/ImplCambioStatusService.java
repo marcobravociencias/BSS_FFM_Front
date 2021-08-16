a226c675df9dd7cd4abf92459d8c59cf31128c94
Héctor Santamaría <hector.stamaria92@gmail.com>
2021-08-16 14:20:21 -0500

    Cambios en el back

--- src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/service/impl/ImplCambioStatusService.java
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/plantainterna/service/impl/ImplCambioStatusService.java
@@ -49,60 +49,43 @@ public class ImplCambioStatusService implements CambioStatusService {
 		LoginResult principalDetail=utilerias.obtenerObjetoPrincipal();
 		
 		jsonObject.addProperty("idUsuarioDespacho", principalDetail.getIdUsuario());
-		jsonObject.addProperty("geer", "asdsassda");
 
-
- /**       JsonObject paramsObject = new JsonObject();
-        paramsObject.addProperty("folioSistema", jsonObject.get("folioSistema").getAsString());
-        paramsObject.addProperty("idFlujo", jsonObject.get("idFlujo").getAsInt());
-        paramsObject.addProperty("idTipoOrden", jsonObject.get("idTipoOrden").getAsInt());
-        paramsObject.addProperty("idSubTipoOrden", jsonObject.get("idSubTipoOrden").getAsInt());
-        paramsObject.addProperty("idOrigenSistema", jsonObject.get("idOrigenSistema").getAsInt());
-        paramsObject.addProperty("idUsuarioDespacho", principalDetail.getIdUsuario());
-        paramsObject.addProperty("idUsuarioTecnico", jsonObject.get("idUsuarioTecnico").getAsInt());
-        paramsObject.addProperty("latitud", jsonObject.get("latitud").getAsInt());
-        paramsObject.addProperty("longitud", jsonObject.get("longitud").getAsInt());
-        paramsObject.addProperty("comentarios", jsonObject.get("comentarios").getAsString());**/
         
-        
+        String baseUrlService=login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts());
         switch (jsonObject.get("tipo").getAsString()) {
             case "reagendamiento":
                 requestTipo = "patch";
-                urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.reagendamiento"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.reagendamiento"));
                 //urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.reagendamiento"));
                 break;
             case "calendariza":
                 requestTipo = "patch";
                 //urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.calendariza"));
-                urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.calendariza"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.calendariza"));
                 break;
             case "termina":
                 requestTipo = "patch";
                 //urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.termina"));
-                urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.termina"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.termina"));
                 break;
             case "desasigna":
                 requestTipo = "patch";
-                urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.desasigna"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.desasigna"));
                 break;
             case "cancela":
                 requestTipo = "patch";
                 //urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.cancela"));
-                urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.cancela"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.cancela"));
 
                 break;
             case "reasigna":
                 requestTipo = "patch";
-                urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.reasigna"));
-            /**    paramsObject.addProperty("fechaHoraInicio", jsonObject.get("idMotivo").getAsString());
-                paramsObject.addProperty("fechaHoraFin", jsonObject.get("idTurno").getAsString());
-                paramsObject.addProperty("idtipoAsignacion", jsonObject.get("idtipoAsignacion").getAsInt());
-             	**/
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.reasigna"));      
                 break;
             case "asigna":
                 requestTipo = "post";
                 //urlRequest = login.getDireccionAmbiente().concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.asigna"));
-                urlRequest = "http://94.74.70.52".concat(constCambioStatus.getCambioDeStatusOts()).concat(environment.getProperty("param.textus.asigna"));
+                urlRequest = baseUrlService.concat(environment.getProperty("param.textus.asigna"));
                 http://94.74.70.52/
                 break;
         }
@@ -119,31 +102,5 @@ public class ImplCambioStatusService implements CambioStatusService {
 
         return response;
     }
-    /**
-	@Override
-	public ServiceResponseResult cambiarEstatusTecnicoPI(String params) {
-		logger.info("ImplDespachoPIService.class [metodo = cambiarEstatusTecnicoPI() ]\n"+params);
-	
-		JsonObject jsonObject=gson.fromJson(params, JsonObject.class);
-		
-		String idUsuario=jsonObject.get("idUsuario").getAsString();
-		String idEstatus=jsonObject.get("idEstatusUsuario").getAsString();
-		
-	    LoginResult principalDetail=utilerias.obtenerObjetoPrincipal();
-	    
-	    logger.info("json object params## "+jsonObject.toString()); 
-	    
-	    String tokenAcces=principalDetail.getAccess_token();
-	    String url=principalDetail.getDireccionAmbiente().concat(constDespachoPI.getCambiarEstatusTecPi());
-	    logger.info("URL ##+" + url);
-	    
-	    Map<String, String> paramsRequestGet = new HashMap<String, String>();
-        paramsRequestGet.put("idUsuario", idUsuario);
-        paramsRequestGet.put("idEstatusUsuario", idEstatus);
-	    
-	    ServiceResponseResult response= restCaller.callPostBearerTokenRequestURL(paramsRequestGet, url,
-	    		ServiceResponseResult.class, tokenAcces);
-	    logger.info("##### RESULT"+gson.toJson(response)+" #######");
-		return response;
-	}**/
+ 
 }
