3cd4f988633a1f1d359531c872fb879a82ba6db1
Héctor Santamaría <hector.stamaria92@gmail.com>
2022-01-12 12:29:32 -0600

    Se agrega validación de llaves

--- src/main/java/com/mx/totalplay/ffm/cloudweb/utilerias/model/Permiso.java
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/utilerias/model/Permiso.java
@@ -1,6 +1,7 @@
 package com.mx.totalplay.ffm.cloudweb.utilerias.model;
 
 import java.util.List;
+import java.util.Map;
 
 import lombok.Builder;
 import lombok.Data;
@@ -20,5 +21,5 @@ public class Permiso {
 	private int id;
 	private int nivel;
 	private int idPadre;
-	private List<ConfiguracionesGenerales> llaves;
+	private Map<String ,Object> llaves;
 }
