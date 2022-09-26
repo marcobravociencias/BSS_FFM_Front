42ca5692935cdcdd2a268cce5358714e544d4b60
Alberto Escobar <jose.escobarr@totalplay.com.mx>
2022-09-26 18:29:57 -0500

    [CREATE]Parametros de Bandejas Eim

---
+++ src/main/java/com/mx/totalplay/ffm/cloudweb/projectmanager/model/ParamFFMBandejasEimVO.java
@@ -0,0 +1,31 @@
+package com.mx.totalplay.ffm.cloudweb.projectmanager.model;
+
+import java.util.List;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder 
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParamFFMBandejasEimVO {
+
+	private String idOrdenTrabajo;
+	private String idClaveCliente;
+	private String folioSistema;
+	private List<Integer> idEstatus;
+	private List<Integer> idEstados;
+	private List<Integer> idGeografias;
+	private String start;
+	private String end;
+	private String fechaInicio;
+	private String fechaFin;
+	private Integer elementosPorPagina;
+	private Integer pagina;
+	private int draw;
+	private String fechaSeleccionada;
+}
