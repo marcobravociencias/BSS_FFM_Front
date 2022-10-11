3b5143434b073755a8e638814b4b81ec259cf6b7
MarcoBravo <mbravo@phinder.mx>
2022-10-10 23:45:25 -0500

    [ADD] OTS BANDEJAS

--- src/main/webapp/resources/js/projectmanager/bandejasEim/bandejasEimController.js
+++ src/main/webapp/resources/js/projectmanager/bandejasEim/bandejasEimController.js
@@ -735,18 +735,41 @@ app.controller('bandejasEimController', ['$scope', '$q', 'coordInstalacionesPISe
 		);
 	}
 	function formatEnImplementacion(d) {
-		//console.log(d);
+		console.log(d);
+		var html = "";
+		for(var i=0; i<d.length; i++){
+			html += `<tr>
+			  	<td>`+d[i].idOrden+`</td>
+				<td>`+d[i].descTipo+`</td>
+				<td>`+d[i].descSubTipo+`</td>
+				<td>`+d[i].fechaCreacion+`</td>
+				<td>`+d[i].tecnico+`</td>
+				<td>`+d[i].nombreCliente+`</td>
+				<td>`+d[i].auxAsig+`</td>
+				<td>`+d[i].descripcionEstatus+`</td>
+				<td>`+d[i].fechaHoraInicio+`</td>
+			  
+			  
+			  
+			 
+			</tr>`;
+
+
+		}
 		return (
 			'<div class="col-8 table-responsive" style="margin-top: 1em; padding: 0;">'+
-			'<table id="myTable" class="table">' +
-			'<thead id="" style="background-color: blue;">'+
-			'<tr><th>OT</th><th>Categoria</th><th>Subcategoria</th><th>Fecha de Creación</th><th>Técnico Asignado</th><th>Solicitante</th><th>Auxiliar</th><th>Estatus</th><th>Hora de llegada</th></tr>'	+
+			'<table>' +
+			'<thead> '+
+			'<tr><th>OT</th><th>Categoria</th><th>Subcategoria</th><th>Fecha de Creación</th><th>Técnico Asignado</th><th>Solicitante</th><th>Auxiliar</th><th>Estatus</th><th>Hora de llegada</th></tr>'+
 			'</thead>'+
-			'<tbody id="myTableEnImplementacion">'+
+			'<tbody id="myTables">'+
+			html+
+			'</tbody>'+
 			'</table>'+
 			'</div>'
 
 		);
+
 	}
 	$scope.consultarPendientesPorImplementar = function(isSwal){
 		pCelula = $("#pCelula").val();
@@ -970,7 +993,50 @@ app.controller('bandejasEimController', ['$scope', '$q', 'coordInstalacionesPISe
 		})
 				
 	}
-	///
+
+	setTimeout(
+		function () {
+			$('#tableDependencia tbody').on('click', 'td.sorting_1', function (){
+
+				//console.log("click en al tabla");
+				var tr = $(this).closest('tr');
+				var row = tableDependencia.row(tr); //mismo nombre que tabla
+				let index = Number($(this).attr('tag-position'));
+
+				//console.log(tr, row)
+
+				if (row.child.isShown()) {
+					row.child.hide();
+				}else {
+					var listaDetalleOs = [] ;
+					console.log('Se ejecuto el else');
+					//row.child(format(row.data())).show();
+					osConsulta = row.data()[7];
+					console.log(osConsulta);
+
+					let params = {
+						os: osConsulta,
+					}
+					genericService.localizaOrden(params).then(function success(response){
+						if(response.data !== undefined){
+							if(response.data.respuesta){
+								if (response.data.result) {
+									if (response.data.result.detalleOS) {
+										listaDetalleOs = response.data.result.detalleOS;
+										row.child(format(listaDetalleOs)).show();
+
+									}
+								}
+							}
+						}
+					})
+
+				}
+
+			});
+		},
+		2000);
+
 	
 	//Table Implentacion
 	$scope.consultarImplementacion = function(isSwal){
@@ -1061,16 +1127,19 @@ app.controller('bandejasEimController', ['$scope', '$q', 'coordInstalacionesPISe
 	}
 	setTimeout(
 		function () {
-			$('#tableimplementacion tbody').on('click', 'tr', function (){
+			$('#tableimplementacion tbody').on('click', 'td.sorting_1', function (){
 
 				//console.log("click en al tabla");
 				var tr = $(this).closest('tr');
 				var row = tableimplementacion.row(tr); //mismo nombre que tabla
+				let index = Number($(this).attr('tag-position'));
 
+				//console.log(tr, row)
 
 				if (row.child.isShown()) {
 					row.child.hide();
 				}else {
+					var listaDetalleOs = [] ;
 					console.log('Se ejecuto el else');
 					//row.child(format(row.data())).show();
 					osConsulta = row.data()[6];
@@ -1080,40 +1149,17 @@ app.controller('bandejasEimController', ['$scope', '$q', 'coordInstalacionesPISe
 						os: osConsulta,
 					}
 					genericService.localizaOrden(params).then(function success(response){
-						$scope.listaDetalleOs = response.data.result.detalleOS;
-						row.child(formatEnImplementacion($scope.listaDetalleOs)).show();
-					})
-					$.each($scope.listaDetalleOs, function(i, item) {
-						lista = document.getElementById("myTableEnImplementacion");
-						var tr = document.createElement("tr");
-						var columna1 = document.createElement("th");
-						columna1.innerHTML = item.idOrden;
-						var columna2 = document.createElement("th");
-						columna2.innerHTML = item.descTipo;
-						var columna3 = document.createElement("th");
-						columna3.innerHTML = item.descSubTipo;
-						var columna4 = document.createElement("th");
-						columna4.innerHTML = item.fechaCreacion;
-						var columna5 = document.createElement("th");
-						columna5.innerHTML = item.tecnico;//falta
-						var columna6 = document.createElement("th");
-						columna6.innerHTML = item.nombreCliente;
-						var columna7 = document.createElement("th");
-						columna7.innerHTML = item.auxAsig;
-						var columna8 = document.createElement("th");
-						columna8.innerHTML = item.descripcionEstatus;
-						var columna9 = document.createElement("th");
-						columna9.innerHTML = item.fechaHoraInicio;
-						lista.appendChild(tr);
-						tr.appendChild(columna1);
-						tr.appendChild(columna2);
-						tr.appendChild(columna3);
-						tr.appendChild(columna4);
-						tr.appendChild(columna5);
-						tr.appendChild(columna6);
-						tr.appendChild(columna7);
-						tr.appendChild(columna8);
-						tr.appendChild(columna9);
+						if(response.data !== undefined){
+							if(response.data.respuesta){
+								if (response.data.result) {
+									if (response.data.result.detalleOS) {
+										listaDetalleOs = response.data.result.detalleOS;
+										row.child(formatEnImplementacion(listaDetalleOs)).show();
+
+									}
+								}
+							}
+						}
 					})
 
 				}
